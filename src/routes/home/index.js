1d722437b8151c3712341f32140ede4690d2d29d
Robert Scott <keyboard.operator@gmail.com>
2017-07-16 23:59:47 -0700

    Add test case.

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,12 +1,17 @@
 import { h, Component } from 'preact';
 import style from './style';
 
+import { Form } from 'semantic-ui-react';
+
 export default class Home extends Component {
 	render() {
 		return (
 			<div class={style.home}>
 				<h1>Home</h1>
 				<p>This is the Home component.</p>
+                <Form>
+                    <Form.Checkbox label='Why the errors?'/>
+                </Form>
 			</div>
 		);
 	}
